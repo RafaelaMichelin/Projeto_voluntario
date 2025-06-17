c9b5c3b0f16eba016afb79ad1ceb0f6fce510642
Rafaela Michelin <RafaelaMichelin@gmail.com>
2025-06-17 19:39:51 -0300

    feat: Criando função para validar se todos os campos foram preenchidos

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
+//Conferir se todos os campos obrigatórios estão preenchidos
 
 
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('form');
+  const btnEnviar = document.getElementById('btnEnviar');
+  const camposObrigatorios = form.querySelectorAll('[required]');
+
+
+}
